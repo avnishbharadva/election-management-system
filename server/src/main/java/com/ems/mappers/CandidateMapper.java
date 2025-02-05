bb6afebb1282532b7f927801e5638df26a88ad26
Aayu Parikh <aryanparikh03@gmail.com>
2025-02-05 14:57:26 +0530

    Error of DTO mapper is being solved

--- server/src/main/java/com/ems/mappers/CandidateMapper.java
+++ server/src/main/java/com/ems/mappers/CandidateMapper.java
@@ -1,9 +1,7 @@
 package com.ems.mappers;
 
 import com.ems.dtos.CandidateDTO;
-import com.ems.entities.Candidate;
-import com.ems.entities.Party;
-import com.ems.entities.CandidateAddress;
+import com.ems.entities.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,12 +11,21 @@ import java.util.List;
 public interface CandidateMapper {
     @Mapping(target = "partyId", source = "party")
     @Mapping(target = "candidateAddress", source = "address")
+    @Mapping(target ="electionId",source = "election")
+    @Mapping(target = "bankDetailsId",source = "bankDetails")
     CandidateDTO toCandidateDTO(Candidate candidate);
 
     Candidate toCandidate(CandidateDTO candidateDTO);
 
     default Long mapPartyToId(Party party) {
         return (party != null) ? party.getPartyId() : null;
+    }
+    default Long mapBankToId(BankDetails bankDetails){
+        return (bankDetails!=null)?bankDetails.getBankDetailsId():null;
+    }
+    default Long mapElectionTOElectionId(Election election)
+    {
+        return (election!=null)?election.getElectionId():null;
     }
 
     List<CandidateAddress> mapAddressList(List<CandidateAddress> addressList);
