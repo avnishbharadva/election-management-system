5b222b779cd501dfda1d01e829bea51be5af9ca3
Avnish Bharadva <avnishbharadva@gmail.com>
2025-02-07 11:57:21 +0530

    logging & exception handling

--- server/src/main/java/com/ems/mappers/GlobalMapper.java
+++ server/src/main/java/com/ems/mappers/GlobalMapper.java
@@ -8,16 +8,19 @@ import com.ems.entities.Party;
 import com.ems.entities.Voter;
 import org.mapstruct.Mapper;
 
-import java.util.Set;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface GlobalMapper {
     Voter toVoter(VoterRegisterDTO voterRegisterDTO);
     VoterRegisterDTO toVoterRegisterDTO(Voter voter);
+
     Party toParty(PartyDTO partyDTO);
     PartyDTO toPartyDTO(Party party);
+
     Address toAddress(AddressDTO addressDTO);
     AddressDTO toAddressDTO(Address address);
-    Set<Address> toAddressSet(Set<AddressDTO> addressDTOS);
-    Set<AddressDTO> toAddressDTOSet(Set<Address> addressSet);
+
+    List<Address> toAddressList(List<AddressDTO> addressDTOList);
+    List<AddressDTO> toAddressDTOList(List<Address> addressList);
 }
