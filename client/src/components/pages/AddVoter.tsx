35035adb71d6417fba41b1246ff07421ca27822d
renishp003 <renishpipaliya003@gmail.com>
2025-02-10 10:31:24 +0530

    sidebar changes

--- client/src/components/pages/AddVoter.tsx
+++ client/src/components/pages/AddVoter.tsx
@@ -3,7 +3,7 @@
 const AddVoter = () => {
   return (
     
-        <></>
+        <>hello</>
   )
 }
 
--- client/src/components/ui/Sidebar.tsx
+++ client/src/components/ui/Sidebar.tsx
@@ -10,6 +10,7 @@ import GroupsIcon from "@mui/icons-material/Groups";
 import HowToVoteIcon from "@mui/icons-material/HowToVote";
 import BallotIcon from "@mui/icons-material/Ballot";
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import { Link } from "react-router-dom";
 
 // Styled Sidebar Container
 
@@ -18,13 +19,13 @@ export default function Sidebar() {
   return (
     <SidebarContainer>
       <List>
-        {[{text: "Dashboard", icon: <DashboardIcon />, path: "/dashboard"},
+        {[{text: "Dashboard", icon: <DashboardIcon />, path: "/dashboard/cards"},
           { text: "Candidates", icon: <AccountBoxIcon />, path: "/dashboard/candidate" },
           { text: "Voters", icon: <HowToVoteIcon />, path: "/dashboard/voter" },
           { text: "Party", icon: <GroupsIcon />, path: "/dashboard/party" },
           { text: "Election", icon: <BallotIcon />, path: "/dashboard/election" },
-        ].map(({ text, icon }) => (
-          <ListItem key={text} disablePadding>
+        ].map(({ text, icon,path }) => (
+          <ListItem key={text} disablePadding component={Link} to={path}>
             <StyledListItemButton>
               <StyledListItemIcon>{icon}</StyledListItemIcon>
               <ListItemText
