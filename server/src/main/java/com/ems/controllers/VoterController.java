d7581cda628c710d55da72a7b9a48aa9a1450970
kashyap jani <kashyapjani50@gmail.com>
2025-02-11 17:08:53 +0530

    voter controller commit

--- server/src/main/java/com/ems/controllers/VoterController.java
+++ server/src/main/java/com/ems/controllers/VoterController.java
@@ -7,6 +7,7 @@ import com.ems.exceptions.PartyNotFoundException;
 import com.ems.services.VoterService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
