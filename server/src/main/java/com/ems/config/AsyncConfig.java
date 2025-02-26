9e93022c800959ad3cd7c3ea3027027fba598b11
Avnish Bharadva <avnishbharadva@gmail.com>
2025-02-26 14:55:23 +0530

    implement custom task executor and party update

---
+++ server/src/main/java/com/ems/config/AsyncConfig.java
@@ -0,0 +1,4 @@
+package com.ems.config;
+
+public class AsyncConfig {
+}
--- server/src/main/java/com/ems/mappers/ElectionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ems.mappers;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ElectionMapper {
-
-}
