bbb5b7229c0bb3f2e2c76e5db006312a0c5b157a
Aayu Parikh <aryanparikh03@gmail.com>
2025-02-27 17:03:15 +0530

    Election controller API changes

--- server/src/main/java/com/ems/services/ElectionService.java
+++ server/src/main/java/com/ems/services/ElectionService.java
@@ -1,15 +1,26 @@
 package com.ems.services;
 
 import com.ems.dtos.ElectionDTO;
+
 import com.ems.dtos.ElectionSortDTO;
+
 import com.ems.entities.Election;
+
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ElectionService {
 
     Election saveElection(ElectionDTO electionDTO);
+
     Election updateElection(Long electionId,ElectionDTO electionDTO);
-    List<ElectionSortDTO> getElectionsSorted(String order);
+
+    Page<ElectionSortDTO> getElectionsSorted(String order, int page, int size);
+
+    void deleteElectionById(Long electionId);
+
+    List<ElectionDTO> getAllElection();
 
 }
+
