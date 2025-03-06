5dcececd5879a42c51097a21ca678c96d33e83d3
Aayu Parikh <aryanparikh03@gmail.com>
2025-03-06 10:25:50 +0530

    Minor changes to Security for added count API

--- server/src/main/java/com/ems/config/SecurityConfiguration.java
+++ server/src/main/java/com/ems/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,6 +24,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -31,42 +33,37 @@ public class SecurityConfiguration {
     private final MyUserDetailService userDetailService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers(
-                            "/authenticate/**", "/email/**", "/api/password/**",
-                            "/v3/api-docs/**", "/swagger-ui/**","/redoc.html","/api/counts"
-                    ).permitAll();
-                    registry.requestMatchers("/api/voters/**", "/api/candidate/**").hasAnyRole("STATE", "COUNTY");
-                    registry.requestMatchers("/officers/register/**", "/getAllRoles/**", "/api/elections/**", "/api/party/**").hasRole("STATE");
+                    registry.requestMatchers( "/authenticate/**" , "/email/**" , "/api/password/**").permitAll();
+                    registry.requestMatchers("/api/voters/**","/api/candidate/**").hasAnyRole("STATE","COUNTY");
+                    registry.requestMatchers("/officers/register/**","/getAllRoles/**","/api/elections/**","/api/party/**").hasRole("STATE");
                     registry.anyRequest().authenticated();
                 })
+
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://172.16.16.66:5173",
-                "http://localhost:8082",
-                "http://localhost:8080",
-                "http://172.16.16.63:8082"
-        ));
+        // configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Replace with your allowed origins
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setExposedHeaders(List.of("Authorization", "Content-Type"));
-        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -90,4 +87,5 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
